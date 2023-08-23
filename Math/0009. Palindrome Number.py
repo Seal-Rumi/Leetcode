3e7a587abda08fb158391def123cc98c67b24eb5
Rumi <sealsaby1201@gmail.com>
2023-08-23 18:25:55 +0800

    Stage 1 Math problem.

---
+++ Math/0009. Palindrome Number.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 18:07:30 2023
+
+@author: Seal-Rumi
+"""
+
+def isPalindrome(x):
+    if (str(x)==str(x)[::-1]):
+        return True
+    else:
+        return False
+   
+#Test
+a = input()
+print(isPalindrome(a))
